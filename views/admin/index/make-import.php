6cd0bd5bbe46380364f16f018f24d93c05254f05
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-07-15 00:00:00 +0200

    Normalized form to make import.

--- views/admin/index/make-import.php
+++ views/admin/index/make-import.php
@@ -15,41 +15,41 @@ echo common('bulkimportfiles-nav');
 ?>
 <?php echo flash(); ?>
 
-<h1>
-    <span class="subhead"><?php echo __('Make bulk import files');?></span>
-    <span class="title"><?php echo __('Bulk import files'); ?></span>
-</h1>
+<h1><?= __('Make bulk import files') ?></h1>
 <form method="post" class="make_import_form">
-    <div class="field required">
-        <div class="field-meta">
-            <label for="directory"><?= __('Source directory') ?></label>
-            <a href="#" class="expand" aria-label="<?= __('Expand') ?>"></a>
-            <div class="collapsible">
-                <div class="field-description"><?= __('Enter the absolute path to the directory where files to be imported will be added.') ?>
-                    <?= sprintf(__('The directory can be anywhere on your server. Your root path is: %s'), $_SERVER['DOCUMENT_ROOT']) ?>
+    <section class="ten columns alpha omega">
+        <fieldset id="fieldset-make_import_form">
+            <legend><?= __('Select folder') ?></legend>
+            <div class="field">
+                <div class="two columns alpha">
+                    <?= $this->formLabel('directory', __('Source directory on server')) ?>
+                </div>
+                <div class='inputs five columns'>
+                    <?= $this->formText('directory', BASE_DIR . '/files/import', null) ?>
+                    <p class="explanation">
+                        <?= __('Enter the absolute path to the directory where files to be imported will be added.') ?>
+                        <?= sprintf(__('The directory can be anywhere on your server. Your root path is: %s'), $_SERVER['DOCUMENT_ROOT']) ?>
+                    </p>
+                </div>
+                <div class='inputs three columns omega'>
+                    <div id="save" class="panel">
+                        <?= $this->formSubmit('add-item-submit', __('Check'), array('class' => 'submit big green button check_button')) ?>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="inputs">
-            <input type="text" name="directory" required="required" id="directory" value="<?= BASE_DIR ?>/files/import">
-        </div>
-        <button type="submit" name="add-item-submit" class="check_button"><?= __('Check') ?></button>
-    </div>
-    <div class="field">
-        <div class="field-meta">
-            <label for="delete-file"><?= __('Delete original file from the folder?') ?></label>
-            <a href="#" class="expand" aria-label="<?= __('Expand') ?>"></a>
-            <div class="collapsible">
-                <div class="field-description">
-                    <?= __('Do you want to delete a file from the source directory after it has been imported? If so, the directory must be server-writable.') ?>
+            <div class="field">
+                <div class="two columns alpha">
+                    <?= $this->formLabel('delete_file', __('Delete original file from the folder?')) ?>
+                </div>
+                <div class='inputs five columns omega'>
+                    <?= $this->formCheckbox('delete_file', true, array('checked' => false, 'id' => 'delete-file')) ?>
+                    <p class="explanation">
+                        <?= __('Do you want to delete a file from the source directory after it has been imported? If so, the directory must be server-writable.') ?>
+                    </p>
                 </div>
             </div>
-        </div>
-        <div class="inputs">
-            <input type="hidden" name="delete_file" value="no">
-            <input type="checkbox" name="delete_file" id="delete-file" value="no">
-        </div>
-    </div>
+        </fieldset>
+    </section>
 </form>
 
 <div class="response"></div>
